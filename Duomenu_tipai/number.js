8ecae2cc7beedf1a9f0d3c46b1fd2e75eb0a8fbf
TadasJS <tadas.x@gmail.com>
2024-07-24 18:35:56 +0300

    pirmos pamokos

---
+++ Duomenu_tipai/number.js
@@ -0,0 +1,51 @@
+const pirmas = 5;
+const antras = 7;
+const trecias = -2.72;
+const nulis = 0;
+const ads = 44444;
+
+console.log(3.1415);
+console.log(antras);
+console.log(trecias);
+console.log(nulis);
+
+const suma = pirmas + antras;
+
+console.log(suma);
+
+console.clear();
+
+let x = 1;
+
+
+
+x += 1;
+console.log(x);
+
+x *= 20;
+console.log(x);
+
+x /= 5;
+console.log(x);
+
+x %= 3;
+console.log(x);
+
+console.clear();
+
+let y = 10;
+
+console.log(y);
+
+y += 10+10+10;
+console.log(y);
+
+y -= 5 * 4;
+console.log(y);
+
+y -= 10 + 5 * 4;
+console.log(y);
+ 
+
+
+
---
+++ Duomenu_tipai/string.js
@@ -0,0 +1,35 @@
+console.log(43434);
+console.clear();
+const name = 'Jonas';
+console.log(name);
+
+const name2 = "Maryte"
+console.log(name2);
+
+const sakinys = 'Labas rytas, Lietuva!';
+console.log(sakinys);
+
+// vienguba (') kabute.
+
+const kabutes1 = "Vienguba (') kabute ";
+console.log(kabutes1);
+
+//Dviguba(") kabute.
+const kabutes2 = 'Dviguba (") kabutes1.';
+console.log(kabutes2);
+
+const personName = 'Chuck';
+const personSurname = 'Norris';
+const fullname = personName + ' '+ personSurname;
+console.log(fullname);
+
+//Vienguba (') ir dviguba (") kabutes
+const kabutes12 = "Vienguba (') ir " +  'dviguba (") kabutes';
+console.log(kabutes12);
+
+//Vienguba (') ir dviguba (") kabutes
+const kabutes12_1 = ' Vienguba (\') ir " +  \'dviguba (\") kabutes';
+const kabutes12_2 = " ";
+console.log(kabutes12_1);
+console.log(kabutes12_2);
+
